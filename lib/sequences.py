758d69df8737b42bced07a85db100fc67abc0186
Imelda Hope <imeldalarusso@gmail.com>
2024-08-30 13:40:33 +0300

    creates a fibonnaci sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    n1,n2 = 0,1    
+
+    my_list = []
+    for _ in range(0, length):
+        my_list.append(n1)
+        n1,n2 = n2, n1 + n2
+            
+    print(my_list)        
+
